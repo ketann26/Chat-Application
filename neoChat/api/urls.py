86a52a5ac479803961bfaa2ee19f302778850104
ketann26 <ketan9sharma@yahoo.in>
2023-06-30 14:13:23 +0530

    Suggest Friends API

--- neoChat/api/urls.py
+++ neoChat/api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from knox import views as knox_views
 
 from . import views
+from .views import SuggestFriends
 
 urlpatterns = [
     path('login/',views.login_api),
     path('register/',views.register_api),
     path('online-users/',views.get_online_users_api),
     path('chat/start/',views.start_chat_api),
+    path('suggested-friends/<int:id>/',SuggestFriends.as_view()),
     path('logout/', knox_views.LogoutView.as_view()),
 ]
--- neoChat/api/views.py
+++ neoChat/api/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+
+import json
 
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.reverse import reverse
@@ -87,6 +91,46 @@ def start_chat_api(request):
         return Response({
             'message': 'User does not exist'
         })
+
+
+class SuggestFriends(APIView):
+
+    def get(self, request, *args, **kwargs):     
+
+        file_data = open('static/users.json')
+        users = json.load(file_data)
+        users_list = users['users']
+        
+        curr_user_id = self.kwargs["id"]
+        for user in users_list:
+            if user['id'] == curr_user_id:
+                curr_user = user
+                break
+
+        user_interest_list = list(curr_user['interests'].keys())
+        user_scores = {}
+        for user in users_list:
+            
+            score = 0
+            if abs(user['age']-curr_user['age']) < 5:
+
+                for interest in user['interests']:
+                    if interest in user_interest_list:
+                        score = score + user['interests'][interest]
+
+                user_scores[user['id']] = score
+
+        user_scores[curr_user_id] = 0
+
+        sorted_user_scores = dict(sorted(user_scores.items(), key=lambda x:x[1],reverse=True))
+        suggested_id_list = list(sorted_user_scores)[:5]
+
+        suggested_friends = []
+        for f_id in suggested_id_list:
+            friend = next(item for item in users_list if item["id"] == f_id)
+            suggested_friends.append(friend)
+
+        return Response({"suggested_friends": suggested_friends})
 
 
     
